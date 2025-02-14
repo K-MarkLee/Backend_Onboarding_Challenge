ab5a4a99280fb9420b46c68ede97ef86980d2e78
K-MarkLee <seungyeol6825@gmail.com>
2025-02-15 01:00:47 +0900

    add : new test cases(jwt validation, access/refresh senario), fix following test result

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from . import views
+from rest_framework_simplejwt.views import TokenRefreshView
+
 
 app_name = "accounts"
 
 urlpatterns = [
     path('signup', views.SignUpView.as_view(), name='signup'),
     path('login', views.LoginView.as_view(), name='login'),
+    path('refresh', views.RefreshView.as_view(), name='refresh'),
 ]
